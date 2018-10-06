0e3ed241adace77048a2dd90879327dab526a2e0
Grace Choi <oiograceoio@gmail.com>
2018-10-06 02:23:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   lowest = 0
-  lowest_key = ""
+  lowest_key = nil
   name_hash.each do |key, value|
     if lowest == nil || value < lowest
       lowest = value
